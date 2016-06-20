d2c98fbe5721a4aeb46bc70da7201c262010c1ad
Robert Ozimek <robertozimek90@gmail.com>
2016-06-20 14:03:05 -0400

    Initial commit

---
+++ threedsnake.ino
@@ -0,0 +1,21 @@
+/*
+ *  3D Snake LED CUBE
+ *  
+ *  HARDWARE:
+ *  74HC595 Shift Registers - led column control
+ *  MOSFET - led layer control
+ *  6 buttons - control the snake
+ *  
+ *  SOURCES
+ *  ShiftPWM Library by Elco Jacobs http://www.elcojacobs.com/shiftpwm/
+ *  
+ */
+
+void setup() {
+
+
+}
+
+void loop() {
+
+}
