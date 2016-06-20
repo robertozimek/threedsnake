a5bcb0645312912fad807cb1fd232cfd6adf5c29
Robert Ozimek <robertozimek90@gmail.com>
2016-06-20 14:15:50 -0400

    Snake setup & setup pin modes and shiftPWM

--- threedsnake.ino
+++ threedsnake.ino
@@ -46,10 +46,71 @@ int backPin = 18;
 // LED cube layer pins
 int layerPins[4] = {4, 5, 6, 7};
 
+boolean gameStarted = false; // Boolean used to start game when button is pressed
+
+// Snake body array
+// -----------------------------
+// (column, level, direction change)
+// -1 - no snake body at array position
+//
+// directions changes at position
+// 0 - forward
+// 1 - backward
+// 2 - up
+// 3 - left
+// 4 - down
+// 5 - right
+int snake[10][3] = {
+  { 27,  2,  0},  // (column, level in array, direction change) 
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1},
+  {  -1,  -1, -1}
+};
+
+int snakeLength = 1;
+
+// Random point for snake to eat
+int randomPoint[2] = {-1, -1}; // column, level
+
+// Snake speed
+int snakeSpeed = 800; // inital speed
 
 void setup() {
+  // Loops over layer pins and sets to output
+  for (int i = 0; i < 4; i++) {
+    pinMode(layerPins[i], OUTPUT);
+  }
 
+  // Seed random number using noise from analog pin 0
+  randomSeed(analogRead(0));
 
+  // Set up controller pins
+  pinMode(upPin, INPUT);
+  pinMode(rightPin, INPUT);
+  pinMode(downPin, INPUT);
+  pinMode(leftPin, INPUT);
+  pinMode(forwardPin, INPUT);
+  pinMode(backPin, INPUT);
+
+  // Hardware interrupt for buttons
+  attachInterrupt(digitalPinToInterrupt(upPin), upPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(rightPin), rightPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(downPin), downPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(leftPin), leftPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(forwardPin), forwardPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(backPin), backPressed, RISING);
+
+  // Set number of registers used
+  ShiftPWM.SetAmountOfRegisters(numRegisters);
+
+  // Start the shift PWM 
+  ShiftPWM.Start(pwmFrequency,maxBrightness);
 }
 
 void loop() {
